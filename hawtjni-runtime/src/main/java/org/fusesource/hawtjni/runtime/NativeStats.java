f8238e452da0602270d1af94ca0769ff6833f2e7
Hiram Chirino <hiram@hiramchirino.com>
2009-11-10 09:26:08 -0500

    Cleaning up the stats class a bit.

--- hawtjni-runtime/src/main/java/org/fusesource/hawtjni/runtime/NativeStats.java
+++ hawtjni-runtime/src/main/java/org/fusesource/hawtjni/runtime/NativeStats.java
@@ -10,8 +10,9 @@
 package org.fusesource.hawtjni.runtime;
 
 import java.io.PrintStream;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -24,15 +25,16 @@ import java.util.Map.Entry;
  * <li> Add the following code around the sections of
  *      interest to dump the native calls done in that section. 
  *      <code><pre>
- *      NativeStats stats = new NativeStats(); ... <code section>
- *      ...
- *      stats.dumpDiff(System.out);
+ *      StatsInterface si = MyFooStatsInterface.INSTANCE;
+ *      NativeStats stats = new NativeStats(si); 
+ *      ... // your code
+ *      stats.diff().dump(System.out);
  *      </pre></code>
  * </li>
  * <li> Or add the following code at a given point to dump a snapshot of
  *      the native calls done until that point.
  *      <code><pre>
- *      new NativeStats().dumpSnapshot(System.out);
+ *      stats.snapshot().dump(System.out);
  *      </pre></code>
  * </li>
  * </ol>
@@ -41,133 +43,178 @@ import java.util.Map.Entry;
  */
 public class NativeStats {
 
-    HashMap<String, ArrayList<NativeFunction>> snapshot;
-
-    final static String[] classes = new String[] { "OS", "ATK", "CDE", "GNOME", "GTK", "XPCOM", "COM", "AGL", "Gdip", "GLX", "Cairo", "WGL" };
-
+    public interface StatsInterface {
+        String getNativeClass();
+        int functionCount();
+        String functionName(int ordinal);
+        int functionCounter(int ordinal);
+    }
+    
     public static class NativeFunction implements Comparable<NativeFunction> {
-        
-        final String name;
-        int callCount;
+        private final int ordinal;
+        private final String name;
+        private int counter;
 
-        public NativeFunction(String name, int callCount) {
+        public NativeFunction(int ordinal, String name, int callCount) {
+            this.ordinal = ordinal;
             this.name = name;
-            this.callCount = callCount;
+            this.counter = callCount;
         }
-
         void subtract(NativeFunction func) {
-            this.callCount -= func.callCount;
+            this.counter -= func.counter;
         }
-
-        public int getCallCount() {
-            return callCount;
+        
+        public int getCounter() {
+            return counter;
         }
-
+        public void setCounter(int counter) {
+            this.counter = counter;
+        }
+        
         public String getName() {
             return name;
         }
 
+        public int getOrdinal() {
+            return ordinal;
+        }
+        
         public int compareTo(NativeFunction func) {
-            return func.callCount - callCount;
+            return func.counter - counter;
+        }
+        
+        public void reset() {
+            counter=0;
+        }
+        
+        public NativeFunction copy() {
+            return new NativeFunction(ordinal, name, counter);
         }
     }
 
-    public NativeStats() {
-        snapshot = snapshot();
+    private final HashMap<StatsInterface, ArrayList<NativeFunction>> snapshot;
+    
+    public NativeStats(StatsInterface... classes) {
+        this(Arrays.asList(classes)); 
     }
 
-    public HashMap<String, ArrayList<NativeFunction>> diff() {
-        HashMap<String, ArrayList<NativeFunction>> newSnapshot = snapshot();
-        for (Entry<String, ArrayList<NativeFunction>> entry : newSnapshot.entrySet()) {
-            String className = entry.getKey();
-            ArrayList<NativeFunction> newFuncs = entry.getValue();
-            ArrayList<NativeFunction> funcs = snapshot.get(className);
-            if (funcs != null) {
-                for (int i = 0; i < newFuncs.size(); i++) {
-                    newFuncs.get(i).subtract(funcs.get(i));
-                }
-            }
-        }
-        return newSnapshot;
+    public NativeStats(Collection<StatsInterface> classes) {
+        this(snapshot(classes)); 
     }
-
-    public void dumpDiff(PrintStream ps) {
-        dump(diff(), ps);
-    }
-
-    public void dumpSnapshot(PrintStream ps) {
-        dump(snapshot(), ps);
-    }
-
-    public void dumpSnapshot(String className, PrintStream ps) {
-        HashMap<String, ArrayList<NativeFunction>> snapshot = new HashMap<String, ArrayList<NativeFunction>>();
-        snapshot(className, snapshot);
-        dump(className, snapshot.get(className), ps);
-    }
-
-    public void dump(HashMap<String, ArrayList<NativeFunction>> snapshot, PrintStream ps) {
-        for (Entry<String, ArrayList<NativeFunction>> entry : snapshot.entrySet()) {
-            dump(entry.getKey(), entry.getValue(), ps);
-        }
-    }
-
-    void dump(String className, ArrayList<NativeFunction> funcs, PrintStream ps) {
-        if (funcs == null)
-            return;
-        Collections.sort(funcs);
-        int total = 0;
-        for (NativeFunction func : funcs) {
-            total += func.getCallCount();
-        }
-        ps.print(className);
-        ps.print("=");
-        ps.print(total);
-        ps.println();
-        for (NativeFunction func : funcs) {
-            if (func.getCallCount() > 0) {
-                ps.print("\t");
-                ps.print(func.getName());
-                ps.print("=");
-                ps.print(func.getCallCount());
-                ps.println();
-            }
-        }
+    
+    private NativeStats(HashMap<StatsInterface, ArrayList<NativeFunction>> snapshot) {
+        this.snapshot = snapshot;
     }
 
     public void reset() {
-        snapshot = snapshot();
-    }
-
-    public HashMap<String, ArrayList<NativeFunction>> snapshot() {
-        HashMap<String, ArrayList<NativeFunction>> snapshot = new HashMap<String, ArrayList<NativeFunction>>();
-        for (int i = 0; i < classes.length; i++) {
-            String className = classes[i];
-            snapshot(className, snapshot);
+        for (ArrayList<NativeFunction> functions : snapshot.values()) {
+            for (NativeFunction function : functions) {
+                function.reset();
+            }
         }
-        return snapshot;
+    }
+    
+    public void update() {
+        for (Entry<StatsInterface, ArrayList<NativeFunction>> entry : snapshot.entrySet()) {
+            StatsInterface si = entry.getKey();
+            for (NativeFunction function : entry.getValue()) {
+                function.setCounter( si.functionCounter(function.getOrdinal()) );
+            }
+        }
+    }
+    
+    public NativeStats snapshot() {
+        NativeStats copy = copy();
+        copy.update();
+        return copy;
     }
 
-    public HashMap<String, ArrayList<NativeFunction>> snapshot(String className, HashMap<String, ArrayList<NativeFunction>> snapshot) {
-        try {
-            Class<?> clazz = getClass();
-            Method functionCount = clazz.getMethod(className + "_GetFunctionCount", new Class[0]);
-            Method functionCallCount = clazz.getMethod(className + "_GetFunctionCallCount", new Class[] { int.class });
-            Method functionName = clazz.getMethod(className + "_GetFunctionName", new Class[] { int.class });
-            
-            int count = ((Integer) functionCount.invoke(clazz, new Object[]{})).intValue();
-            ArrayList<NativeFunction> funcs = new ArrayList<NativeFunction>(count);
-            for (int i = 0; i < count; i++) {
-                Object[] args = new Object[]{i};
-                int callCount = ((Integer) functionCallCount.invoke(clazz, args)).intValue();
-                String name = (String) functionName.invoke(clazz, args);
-                funcs.add( new NativeFunction(name, callCount) );
+    public NativeStats copy() {
+        HashMap<StatsInterface, ArrayList<NativeFunction>> rc = new HashMap<StatsInterface, ArrayList<NativeFunction>>(snapshot.size()*2);
+        for (Entry<StatsInterface, ArrayList<NativeFunction>> entry : snapshot.entrySet()) {
+            ArrayList<NativeFunction> list = new ArrayList<NativeFunction>(entry.getValue().size());
+            for (NativeFunction function : entry.getValue()) {
+                list.add(function.copy());
+            }
+            rc.put(entry.getKey(), list);
+        }
+        return new NativeStats(rc);
+    }
+    
+    public NativeStats diff() {
+        HashMap<StatsInterface, ArrayList<NativeFunction>> rc = new HashMap<StatsInterface, ArrayList<NativeFunction>>(snapshot.size()*2);
+        for (Entry<StatsInterface, ArrayList<NativeFunction>> entry : snapshot.entrySet()) {
+            StatsInterface si = entry.getKey();
+            ArrayList<NativeFunction> list = new ArrayList<NativeFunction>(entry.getValue().size());
+            for (NativeFunction original : entry.getValue()) {
+                NativeFunction copy = original.copy();
+                copy.setCounter( si.functionCounter(copy.getOrdinal()) );
+                copy.subtract(original);
+                list.add(copy);
+            }
+            rc.put(si, list);
+        }
+        return new NativeStats(rc);
+    }
+
+    /**
+     * Dumps the stats to the print stream in a JSON format.
+     * @param ps
+     */
+    public void dump(PrintStream ps) {
+        boolean firstSI=true;
+        for (Entry<StatsInterface, ArrayList<NativeFunction>> entry : snapshot.entrySet()) {
+            StatsInterface si = entry.getKey();
+            ArrayList<NativeFunction> funcs = entry.getValue();
+
+            int total = 0;
+            for (NativeFunction func : funcs) {
+                total += func.getCounter();
             }
             
-            snapshot.put(className, funcs);
-            
-        } catch (Throwable e) {
-            // e.printStackTrace(System.out);
+            if( !firstSI ) {
+                ps.print(", ");
+            }
+            firstSI=false;
+            ps.print("[");
+            if( total>0 ) {
+                ps.println("{ ");
+                ps.println("  \"class\": \""+si.getNativeClass()+"\",");
+                ps.println("  \"total\": "+total+", ");
+                  ps.print("  \"functions\": {");
+                boolean firstFunc=true;
+                for (NativeFunction func : funcs) {
+                    if (func.getCounter() > 0) {
+                        if( !firstFunc ) {
+                            ps.print(",");
+                        }
+                        firstFunc=false;
+                        ps.println();
+                        ps.print("    \""+func.getName()+"\": "+func.getCounter());
+                    }
+                }
+                ps.println();
+                ps.println("  }");
+                ps.print("}");
+            }            
+            ps.print("]");
         }
-        return snapshot;
     }
+
+    static private HashMap<StatsInterface, ArrayList<NativeFunction>> snapshot(Collection<StatsInterface> classes) {
+         HashMap<StatsInterface, ArrayList<NativeFunction>> rc = new HashMap<StatsInterface, ArrayList<NativeFunction>>();
+        for (StatsInterface sc : classes) {
+            int count = sc.functionCount();
+            ArrayList<NativeFunction> functions = new ArrayList<NativeFunction>(count);
+            for (int i = 0; i < count; i++) {
+                String name = (String) sc.functionName(i);
+                functions.add(new NativeFunction(i, name, 0));
+            }
+            Collections.sort(functions);
+            rc.put(sc, functions);
+        }
+        return rc;
+    }
+    
+
 }

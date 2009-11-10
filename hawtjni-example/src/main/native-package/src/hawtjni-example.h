4fd292f64153c6d75902bbe83bb22b6f970000d7
Hiram Chirino <hiram@hiramchirino.com>
2009-11-10 07:13:58 -0500

    Adding copyright headers

--- hawtjni-example/src/main/native-package/src/hawtjni-example.h
+++ hawtjni-example/src/main/native-package/src/hawtjni-example.h
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Progress Software, Inc.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 #ifndef INCLUDED_PLATFORM_H
 #define INCLUDED_PLATFORM_H
 
